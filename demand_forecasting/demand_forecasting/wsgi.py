631bf5409a6948844949e195b809a647f55b612a
Sriram Ramakrishnan <srirampro107@gmail.com>
2023-08-14 19:29:02 +0530

    Created Project demand_forecasting

---
+++ demand_forecasting/demand_forecasting/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for demand_forecasting project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demand_forecasting.settings')
+
+application = get_wsgi_application()
