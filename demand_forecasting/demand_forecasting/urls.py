c7777e737add3699466759cad89b04ad1e7d8dcf
Ravin D <ravin.d3107@outlook.com>
2023-08-14 20:25:16 +0530

    Done with Homepage setup and Static files as well

--- demand_forecasting/demand_forecasting/urls.py
+++ demand_forecasting/demand_forecasting/urls.py
@@ -19,6 +19,9 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
+from webApp import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.homepage, name='home'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
