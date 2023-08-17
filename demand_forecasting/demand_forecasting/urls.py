69dfec79825967eab493cca719f5c802a404c370
Sriram Ramakrishnan <srirampro107@gmail.com>
2023-08-17 11:53:05 +0530

    Updated Database

--- demand_forecasting/demand_forecasting/urls.py
+++ demand_forecasting/demand_forecasting/urls.py
@@ -24,5 +24,5 @@ from webApp.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('',views.homepage, name='home'),
-    path('demand/',DFView.as_view(),name='demand')
+    path('',DFView.as_view(),name='')
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
